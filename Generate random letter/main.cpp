d466acb0ddf8d6c4415f210984517f9c699ab59d
Valik <kashko2001v@mail.ru>
2019-03-04 23:40:17 +0300

    up date

--- Generate random letter/main.cpp
+++ Generate random letter/main.cpp
@@ -3,17 +3,36 @@
 #include <algorithm>
 #include <random>
 #include <iterator>
-#include <vector>
+#include <string>
 #include <cstddef>
+#include <ctime>
+#include <fstream>
+
+std::string get_random_string(char first_l, char last_l, int size)
+{
+    std::string res;
+    std::mt19937 gen;
+    gen.seed(time(0));
+    std::uniform_int_distribution<int> uid(first_l,last_l);
+    for(int i = 0; i < size; ++i)
+    {
+     res.push_back(uid(gen));
+    }
+    return res;
+}
 
 int main()
 {
-    std::vector<int> vec(100000);
-    std::iota(vec.begin(), vec.end(),0);
-    //std::shuffle(vec.begin(), vec.end(), std::mt19937{std::random_device{}()});
-    std::mt19937 gen{std::random_device()()};
-    std::uniform_int_distribution<int> uid(0, 10000);
-    std::generate(vec.begin(), vec.end(), [&uid, &gen](){return uid(gen);});
-    std::for_each(vec.begin(), vec.end(), [](int a){std::cout << a << " ";});
+    std::string temp = get_random_string(65, 122, 100);
+    std::cout << temp;
+    std::fstream fs("Random Letter.txt", fs.in | fs.out | fs.trunc);
+    if(!fs.is_open())
+    {
+        std::cout << "error";
+    }
+    else
+    {
+        fs << temp;
+    }
     return 0;
 }
--- test_random/main.cpp
+++ test_random/main.cpp
@@ -9,9 +9,11 @@ int main()
 {
     std::mt19937 gen;
     gen.seed(time(0));
-    std::vector<int> v(10000);
-    std::generate(v.begin(), v.end(), [&gen](){return std::abs(gen()% 667);});
+    std::vector<int> v(100000);
+    //std::generate(v.begin(), v.end(), [&gen](){return std::abs(gen()% 667);});
+    std::uniform_int_distribution<int> uid(0,1000);
+    std::generate(v.begin(), v.end(), [&uid, &gen](){return uid(gen);});
     std::for_each(v.begin(), v.end(), [](int a){std::cout << a << " ";});
-   // std::cout << gen();
+    //std::cout << gen();
     return 0;
 }
